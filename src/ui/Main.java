a0926adf531156359adb60e975a08003253e8d0d
Juan Jose Restrepo Bonilla <juan.restrepo13@correo.icesi.edu.co>
2019-08-18 13:20:15 -0500

    first part of the lab 2

---
+++ src/ui/Main.java
@@ -0,0 +1,36 @@
+package ui;
+import model.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public Main() {
+	}
+	
+	public void showMenu() throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("1.Crear club");
+		System.out.println("2.Salir");
+		String entrada = br.readLine();
+		int inputUser = Integer.parseInt(entrada);
+		
+		while(inputUser != 2) {
+			
+			if(inputUser == 1) {
+				
+			}
+			
+			
+		}//finalDelWhile
+		
+	}//FinalDeShowMenu
+	
+
+	public static void main(String[] args) throws IOException {
+		Main m = new Main();
+		m.showMenu();
+	}
+
+}
